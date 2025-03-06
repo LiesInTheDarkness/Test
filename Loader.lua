5523a22217c22727212249a00ae64d5091c411a5
LiesInTheDarkness <140200653+LiesInTheDarkness@users.noreply.github.com>
2025-03-06 19:16:41 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,13 @@
-local RiseLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/LiesInTheDarkness/Test/main/GuiLibrary.lua"))()
+local scriptContent = game:HttpGet("https://raw.githubusercontent.com/LiesInTheDarkness/Test/main/GuiLibrary.lua")
+if not scriptContent then
+    error("Failed to fetch script content")
+end
+
+local RiseLib = loadstring(scriptContent)()
 if not RiseLib then
     error("Failed to load GuiLibrary")
 end
+
 local window = RiseLib:CreateWindow("My Window", "Classic")
 local tab = window:AddTab("My Tab")
 local category = tab:AddCategory("My Category")
