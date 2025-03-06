fcc81f1bba3d0176a1642bb083f24e0e3a519afe
LiesInTheDarkness <140200653+LiesInTheDarkness@users.noreply.github.com>
2025-03-06 18:55:02 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,8 @@
-local RiseLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/LiesInTheDarkness/Test/refs/heads/main/GuiLibrary.lua"))()
+local RiseLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/LiesInTheDarkness/Test/main/GuiLibrary.lua"))()
+if not RiseLib then
+    error("Failed to load GuiLibrary")
+end
 local window = RiseLib:CreateWindow("My Window", "Classic")
 local tab = window:AddTab("My Tab")
 local category = tab:AddCategory("My Category")
--- Now you can add modules like:
 category:AddToggle("My Toggle", false, function(value) print(value) end)
